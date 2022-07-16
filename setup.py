c635de2e4c5be29ee0c700c4e6966da9e4b039e4
medaka0228 <anywherefish@gmail.com>
2022-07-16 14:49:42 +0900

    インポートするとよくないらしい

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
-import vrw_web_client
 
 setup(
     name='vrw_web_client',
     version='1.1.1',
     packages=find_packages(),
-        install_requires = [
+    install_requires = [
         "requests"
     ]
 )
