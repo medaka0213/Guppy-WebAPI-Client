a99e5c50a8a2523a49455e473a5204bdf976e309
medaka0228 <anywherefish@gmail.com>
2022-07-16 14:41:08 +0900

    ver 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import vrw_web_client
 
 setup(
     name='vrw_web_client',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
         install_requires = [
         "requests"
